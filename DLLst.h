2c1fad9d9c4911c4d0eec61b08c17c79b782784d
pablocontini <31960196+pablocontini@users.noreply.github.com>
2017-10-04 12:33:40 -0300

    Create DLLst.h

---
+++ DLLst.h
@@ -0,0 +1,12 @@
+#ifndef DOUBLY_LINKED_LIST
+#define DOUBLY_LINKED_LIST
+
+#include <iostream>
+
+template<class T>
+class Node {
+public:
+  Node() {
+    next = prev = 0;
+  }
+  Node(const T& el, Node *n = 0, Node *p = 0) {
