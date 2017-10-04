84e4b6d8de7081edbbb4fae7c3beedc8d3dbe97d
pablocontini <31960196+pablocontini@users.noreply.github.com>
2017-10-04 13:10:30 -0300

    Update DLLst.h

--- DLLst.h
+++ DLLst.h
@@ -12,4 +12,44 @@ public:
   Node(const T& el, Node *n = 0, Node *p = 0) {
     info = el; next = n; prev = p;
   }
-  
+  T info;
+  Node *next, *prev;
+},
+
+template<class T>
+class DoublyLinkedList {
+public:
+  DoublyLinkedList() {
+    head = tail = 0;
+  }
+  void addToDLLTail(cont T&);
+  T deleteFromDLLTail();
+protected:
+  Node<T> *head, *tail;
+};
+
+template<class T>
+void DoublyLinkedList<T>::addToDLLTail(const T& el) {
+  if (tail!=0) {
+    tail = new Node<T>(el, 0, tail);
+    tail->prev->next=tail;
+  }
+  else head = tail = new Node<T>(el);
+}
+
+template<class T>
+T DoublyLinkedList<T>::deleteFromDLLTail() {
+  T el = tail->info;
+  if (head == tail) { // if only one node in the list;
+    delete head;
+    head = tail = 0;
+  }
+  else {              // if more than one node in the list;
+    tail = tail->prev;
+    delete tail->next;
+    tail->next = 0;
+  }
+  return el;
+}
+
+#endif
